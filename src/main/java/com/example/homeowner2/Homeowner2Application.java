ba992759f7f9241b6322d5e201dc49596878c8f0
Daoud Himmo <d@ambergrove.homes>
2022-02-11 12:45:01 +0000

    initial commit

---
+++ src/main/java/com/example/homeowner2/Homeowner2Application.java
@@ -0,0 +1,13 @@
+package com.example.homeowner2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homeowner2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homeowner2Application.class, args);
+    }
+
+}
