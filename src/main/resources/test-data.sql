f2f4c03e99395f0f9745906aec302bc04036918f
Daoud Himmo <d@ambergrove.homes>
2022-02-11 13:37:07 +0000

    added test data

---
+++ src/main/resources/test-data.sql
@@ -0,0 +1,5 @@
+Insert into `user`
+(`first_name`, `last_name`, `user_name`, `number_properties`, `net_worth`)
+values('John', 'Smith', 'johnsmith@hotmail.com', 2, 300000);
+-- Insert into user(first_name, last_name, user_name)
+-- values('James', 'Johnson', 'jamesjohnson@hotmail.com');
